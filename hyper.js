897fae7c3d862f8b711f85b2bf23cd41776280cf
Omar Bahareth <omar@omar.engineer>
2019-01-26 16:47:39 +0300

    Switch to nord-hyper theme

--- hyper.js
+++ hyper.js
@@ -57,24 +57,6 @@ module.exports = {
     //colors: {
     //},
 
-    MaterialTheme: {
-      // Set the theme variant,
-      // OPTIONS: 'Darker', 'Palenight', ''
-      theme: 'Darker',
-
-      // [Optional] Set the rgba() app background opacity, useful when enableVibrance is true
-      // OPTIONS: From 0.1 to 1
-      // backgroundOpacity: '0.70',
-
-      // [Optional] Set the accent color for the current active tab
-      accentColor: '#FF408B',
-
-      // [Optional] Mac Only. Need restart. Enable the vibrance and blurred background
-      // OPTIONS: 'dark', 'ultra-dark', 'bright'
-      // NOTE: The backgroundOpacity should be between 0.1 and 0.9 to see the effect.
-      // vibrancy: 'dark'
-    },
-
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
     // make sure to use a full path if the binary name doesn't work
@@ -93,7 +75,7 @@ module.exports = {
     bell: 'SOUND',
 
     // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false
+    copyOnSelect: false,
 
     // if true, on right click selected text will be copied or pasted if no
     // selection is present (true by default on Windows)
@@ -106,7 +88,7 @@ module.exports = {
   },
 
   plugins: [
-    "hyper-material-theme",
+    'nord-hyper',
     'hyperterm-1password',
     'hyper-pane',
     'hyperlinks',
